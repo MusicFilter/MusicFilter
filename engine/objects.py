a97b2bbdd27f8ee1f849e401bfb79822dbf4557c
Yoav Porat <yoavporat@hotmail.com>
2016-05-24 18:17:31 +0300

    Player page and dummy DB

---
+++ engine/objects.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+
+class Playlist:
+
+    """
+    Playlist object
+    self.id = [int] playlist_id from DB
+    self.hits = [int] total_hits from DB
+    self.createdOn = [datetime] creation date from DB
+    self.video_list = [list] of [string] youtube video id
+    self.name = [string] playlist's name
+    self.elapsed = [string] how much time passed since creation
+    """
+
+    """
+    Constructor
+    """
+    def __init__(self, id):
+        # init fields
+        self.id = id
+        self.hits = 0
+        self.createdOn = datetime(2000, 1, 1)
+        self.video_list = []
+        self.name = ''
+
+        self.elapsed = 0
+
+        # populate fields from BD
+        self.fetch()
+
+    """
+    toString
+    """
+    def __str__(self):
+        return 'Playlist[id={0}, hits={1}, createdOn={2}]'.format(self.id, self.hits, self.createdOn)
+
+    """
+    Populate fields from DB
+    Don't fetch video list!
+    """
+    def fetch(self):
+        # TODO
+        pass
+
+    """
+    Fetch video list
+    """
+    def fetch_video_list(self):
+        # TODO
+        pass
+
+    def getElapsed(self):
+        e = datetime.now() - self.createdOn
+
+        # resolution
+        if e.days != 0:
+            self.elapsed = '{0}d ago'.format(e.days)
+
+        elif e.seconds > 3600:
+            self.elapsed = '{0}h ago'.format(int(round(e.seconds / float(3600), 0)))
+
+        elif e.seconds > 60:
+            self.elapsed = '{0}m ago'.format(int(round(e.seconds / float(60), 0)))
+
+        elif e.seconds > 0:
+            self.elapsed = '{0}s ago'.format(e.seconds)
--- website/views.py
+++ website/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
+from engine import backendInterface
+from engine.objects import Playlist
 
-
+"""
+Homepage
+"""
 def home(request):
-    context = {}
+
+    context = {
+        'trending': backendInterface.getTrending(),
+        'newest':   backendInterface.getNewest()
+    }
     return render(request, 'index.html', context)
+
+
+"""
+Player
+"""
+def player(request, playlist_id):
+
+    playlist404 = Playlist(-1)
+    playlist404.name = 'Playlist Not Found'
+    playlist404.video_list = []
+
+    p = backendInterface.getPlaylistById(playlist_id)
+
+    # update hit count
+    backendInterface.incrementHitCount(playlist_id)
+
+    if p == -1:
+        # playlist not found
+        context = {
+            'playlist': playlist404
+        }
+
+    else:
+
+        context = {
+            'playlist': p
+        }
+
+    return render(request, 'player.html', context)
