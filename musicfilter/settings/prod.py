1ce0b8cff62855c69c1b5663daa06c5ada4ebac6
DilbertMarker <DilbertMarker@users.noreply.github.com>
2016-06-07 14:02:34 +0300

    Database settings fix.

--- musicfilter/settings/prod.py
+++ musicfilter/settings/prod.py
@@ -10,7 +10,7 @@ DATABASES = {
         'HOST': 'mysqlsrv.cs.tau.ac.il',
         'PORT': '3306',
         'OPTIONS': {
-            'charset': 'utf8mb4',
+            'charset': 'utf8',
             'init_command': 'SET character_set_client=utf8mb4,character_set_results=utf8mb4,character_set_connection=utf8mb4,collation_connection=utf8mb4_unicode_ci'
         }
     }
